1aa5d45b32fc252dfbf65a61dcb302822bc45990
HiroNaka1211 <njim1211@gmail.com>
2025-03-16 22:55:32 +0900

    add loading

--- frontend/src/components/EditedImages/EditedImages.tsx
+++ frontend/src/components/EditedImages/EditedImages.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import style from "../InputImages/InputImages.module.css";
+import style from "./EditedImages.module.css";
+import "./EditedImages.css";
 
 type EditedImagesProps = {
   editedImages: EditedImageType[];
@@ -21,10 +22,17 @@ export default function EditedImages({
                 <div className={style.card} key={index}>
                   {/* <p>{decodeURIComponent(image.fileName)}</p> */}
                   <img
-                    src={"data:image/png;base64," + image.buffer}
+                    src={
+                      "data:image/p                ng;base64," + image.buffer
+                    }
                     width={300}
                     height={300}
                   />
+                  <div className={style.test}>
+                    <a href={"data:image/png;base64," + image.buffer} download>
+                      DOWNLOAD
+                    </a>
+                  </div>
                 </div>
               );
             })}
